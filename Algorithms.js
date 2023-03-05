2b7d28a538709a1b7503eeebaaaf4dfaf83f13f9
Savan Sutariya <savanpatel5248@gmail.com>
2023-03-05 22:56:08 +0530

    FCFS clear implementation and increment in scaleability

--- Algorithms.js
+++ Algorithms.js
@@ -1,51 +1,93 @@
-var processorDetails = []
-function sortByArrivalTime() {
-    processes_table.sort(function (a, b) {
-        return a.arrivalTime - b.arrivalTime;
-    });
+// get the data from process table to ready queue
+function getProcessData() {
+    var processTable = document.getElementById("process-table-body");
+    var processRows = processTable.rows;
+    var processArray = [];
+    for (var i = 0; i < processRows.length; i++) {
+        var process = {
+            name: processRows[i].cells[0].innerHTML,
+            burstTime: parseInt(processRows[i].cells[1].innerHTML),
+            arrivalTime: parseInt(processRows[i].cells[2].innerHTML)
+        };
+        processArray.push(process);
+    }
+    return processArray;
 }
-function processorLog(name,start,end,signal) {
-    if(signal == 1){
-        const job = {
-            name: name,
-            start: start,
-            end: end,
+
+// execute the algorithm
+function executeAlgorithm(algorithm, quantum = null) {
+    var processArray = getProcessData();
+
+    if (algorithm === "fcfs") {
+        FCFS(processArray);
+    } else if (algorithm === "sjn") {
+        sjf(processArray);
+    } else if (algorithm === "rr") {
+        // check if quantum is a number
+        if (isNaN(quantum)) {
+            alert("Quantum must be a number");
+        } else {
+            rr(processArray, quantum);
         }
-        processorDetails.push(job)
+    } else if (algorithm === "srtn") {
+        srtn(processArray);
     }
 }
-function runFCFS() {
-    // load processes from table
-    if (processes_table.length == 0) {
-        // console.Error("No process in the table")
-        console.error("No process in table")
-        return undefined
+
+function getTotalBurstTime(array) {
+    var time = 0;
+    for (var i = 0; i < array.length; i++) {
+        time += array[i].burstTime;
     }
-    processorDetails = []
-    sortByArrivalTime();
-    var currentTime = processes_table[0].arrivalTime;
-    var totalWaitingTime = 0;
-    var totalTurnaroundTime = 0;
-    for (var i = 0; i < processes_table.length; i++) {
-        var process = processes_table[i];
-        var waitingTime = currentTime - process.arrivalTime;
-
-        if (waitingTime < 0) {
-            waitingTime = 0;
-        }
-        processorLog(process.name,currentTime,currentTime+process.burstTime,1)
-        var turnaroundTime = waitingTime + process.burstTime;
-        totalTurnaroundTime += turnaroundTime;
-        totalWaitingTime += waitingTime;
-        currentTime += process.burstTime;
+    return time;
+}
+function FCFS(processArray) {
+    processArray.sort((a, b) => a.arrivalTime - b.arrivalTime);
+    console.log(processArray);
+    var readyQueue = [];
+    var terminatedProcesses = []
+    const totalBurstTime = getTotalBurstTime(processArray)
+    for (let time = 0; time <= totalBurstTime; time++) {
+        setTimeout(() => {
+            console.log(time)
+            // Add processes to reayqueue
+            if (processArray.length > 0) {
+                for (var i = 0; i < processArray.length; i++) {
+                    // console.log(processArray[i].name+" Time"+time)
+                    if (processArray[i].arrivalTime <= time) {
+                        processArray[i].waitingTime = 0;
+                        processArray[i].turnaroundTime = 0
+                        readyQueue.push(processArray[i]);
+                        processArray.shift();
+                        i--;
+                    }
+                }
+            }
+            console.log(readyQueue);
+            for (var i = 1; i < readyQueue.length; i++) {
+                readyQueue[i].waitingTime++;
+                readyQueue[i].turnaroundTime++;
+                document.getElementById("process-queue-table-body").innerHTML += `
+                            <tr>
+                                <td>${readyQueue[i].name}</td>
+                                <td>${readyQueue[i].burstTime}</td>
+                                <td>${readyQueue[i].arrivalTime}</td>
+                            </tr>
+                        `;
+            }
+            // FCFS Logic
+            if (readyQueue.length > 0) {
+                if (readyQueue[0].burstTime > 0) {
+                    readyQueue[0].turnaroundTime++;
+                    readyQueue[0].burstTime--;
+                }
+                if (readyQueue[0].burstTime == 0) {
+                    //Remove from ready queue and push to terminated process array
+                    terminatedProcesses.push(readyQueue.shift());
+                }
+            }
+        }, time * 1000);
     }
+    console.log(terminatedProcesses);
 
-    var numProcesses = processes_table.length;
-    var avgWaitingTime = totalWaitingTime / numProcesses;
-    var avgTurnaroundTime = totalTurnaroundTime / numProcesses;
-
-    console.log("FCFS results:");
-    console.log("Average waiting time: " + avgWaitingTime);
-    console.log("Average turnaround time: " + avgTurnaroundTime);
-    console.log(processorDetails)
 }
